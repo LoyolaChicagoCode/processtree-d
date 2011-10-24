5f86ec6a90bad8b4aba71e0da15920855475c260
Konstantin Läufer <laufer@cs.luc.edu>
2011-10-24 00:03:33 -0500

    Switched parsing from regex to simple split. Switched to SList for parent-child map.

--- src/main.d
+++ src/main.d
@@ -12,47 +12,38 @@ struct proc {
 int main() {
 	string buf;
 	proc[int] pmap;
-	bool[int][int] tmap;
+	SList!(int)[int] tmap;
 	 
 	void printTree(int l, int i) {
 		writefln("%s%d: %s", replicate(" ", l), pmap[i].pid, pmap[i].cmd);
 		if (i in tmap)
-			foreach (j ; tmap[i].keys) printTree(l + 1, j);
+			foreach (j ; tmap[i]) printTree(l + 1, j);
 	}
 
 	stdin.readln(buf);
 	auto toProc = procFromLine(buf);
 
 	while (stdin.readln(buf)) { auto p = toProc(buf); pmap[p.pid] = p; }
-	foreach (p ; pmap) tmap[p.ppid][p.pid] = true;
-	foreach (i ; tmap[0].keys) printTree(0, i);
+	foreach (p ; pmap) {
+		if (! (p.ppid in tmap)) tmap[p.ppid] = SList!(int)();
+		tmap[p.ppid].insert(p.pid);
+	}
+	foreach (i ; tmap[0]) printTree(0, i);
 	
     return 0;
 }
 
 proc delegate(string) procFromLine(string header) {
-	auto ms = map!((auto m) { return m.hit(); })(match(header, regex("\\S+")));
-	auto ixs = map!((auto h) { return countUntil(ms, h); })(["PID", "PPID", "CMD", "COMMAND"]);
-	auto iPid = ixs[0], iPpid = ixs[1], iCmd = ixs[2], iCommand = ixs[3];
-	auto iFirst = min(iPid, iPpid), iSecond = max(iPid, iPpid), iThird = max(iCmd, iCommand);
-	assert (iPid >= 0);
-	assert (iPpid >= 0);
-	assert (iThird > iSecond);
-	string pattern; 
-	pattern.reserve(iThird * 8);
-	pattern ~= replicate("\\S+\\s+", iFirst);
-	pattern ~= "(\\S+)\\s+";
-	pattern ~= replicate("\\S+\\s+", iSecond - iFirst - 1);
-	pattern ~= "(\\S+)\\s+";
-	pattern ~= replicate("\\S+\\s+", iThird - iSecond - 1);
-	pattern ~= "(\\S.*)";
-	bool pidFirst = iPid < iPpid;
-	auto rx = regex(pattern);
+	auto cols = header.split;
+	auto iPid = cols.countUntil("PID"); 
+	auto iPpid = cols.countUntil("PPID"); 
+	auto iCmd = max(countUntil(cols, "CMD"), countUntil(cols, "COMMAND")); 
+	assert(iPid >= 0);
+	assert(iPpid >= 0);
+	assert(iCmd >= max(iPid, iPpid));
 	proc f(string line) {
-		auto cs = match(line, rx).captures;
-		auto s1 = cs[1], s2 = cs[2];
-		auto first = parse!(int)(s1), second = parse!(int)(s2);
-		return proc(pidFirst ? first : second, pidFirst ? second : first, cs[3]);
+		auto words = line.split;
+		return proc(parse!(int)(words[iPid]), parse!(int)(words[iPpid]), words[iCmd..words.length].join);
 	}
 	return &f;
 }
